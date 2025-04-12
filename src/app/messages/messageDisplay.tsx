7a8fd098aa879d8974b769120ab8881d602f114d
rob croft <rob@robincroft.com>
2025-04-12 12:58:07 +0100

    nav bar

--- src/app/messages/messageDisplay.tsx
+++ src/app/messages/messageDisplay.tsx
@@ -10,9 +10,9 @@ interface MessageDisplayProps {
 
 const MessageDisplay: React.FC<MessageDisplayProps> = ({ messages, loading, selectedAuthor, error }) => {
     return (
-        <main className="w-full overflow-auto"  >
+        <main className="w-full overflow-y-auto"  >
             {/* Messages Tab Content */}
-            <div className="flex flex-col gap-4 max-w-md w-full justify-center align-center mx-auto mt-4 sm:mt-8">
+            <div className="flex flex-col gap-4 max-w-md w-full justify-center align-center mx-auto mt-4">
                 {error && <p className="text-red-500 text-center">{error}</p>}
                 {loading && (
                     <div className="text-center">
